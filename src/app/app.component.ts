06d4a4b0584a3e594e48a63c4540f4958c7309d7
Jeremy Likness <jeremy@jeremylikness.com>
2016-07-09 15:00:11 -0400

    Initial setup

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import '../../public/css/styles.css';
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent { 
+
+    public now: Date; 
+    
+    constructor() {
+        this.now = new Date();
+    }
+}
