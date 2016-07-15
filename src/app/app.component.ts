b98c61bdde3fb978f0ebc5d379e0c15ce30c91c3
Jeremy Likness <jeremy@jeremylikness.com>
2016-07-15 15:04:16 -0400

    auto refresh the time

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import '../../public/css/styles.css';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent { 
+export class AppComponent implements OnInit { 
 
     public now: Date; 
 
     constructor() {
         this.now = new Date();
+    }
+
+    public ngOnInit(): void {
+        setInterval(() => this.now = new Date(), 900);
     }
 }
